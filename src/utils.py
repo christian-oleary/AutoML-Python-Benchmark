28e34e86077e7aaaf155b4f0a6a7ff4f7f6ed266
Christian O'Leary <christian.oleary@mtu.ie>
2023-02-16 13:32:47 +0000

    handle different file encodings and missing starting timestamps

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import os
 import zipfile
 
@@ -21,6 +22,8 @@ class Utils:
         'yearly': '1Y'
     }
 
+    default_start_timestamp = datetime.strptime('1970-01-01 00-00-00', '%Y-%m-%d %H-%M-%S')
+
     @staticmethod
     def format_forecasting_data(data_dir, debug=False):
 
@@ -29,30 +32,41 @@ class Utils:
         if debug: # Testing occurs with one file
             tsf_files = [tsf_files[0]]
 
+        # Parse .tsf files sequentially
         for tsf_file in tsf_files:
             csv_path = os.path.join(data_dir, f'{tsf_file.split(".")[0]}.csv')
 
             if not os.path.exists(csv_path):
                 print(f'Parsing {tsf_file}')
+                # Parse .tsf files and output dataframe
                 data, freq, horizon, has_nans, equal_length = convert_tsf_to_dataframe(
                     os.path.join(data_dir, tsf_file), 'NaN', 'value')
 
+                # Determine frequency
                 if freq is not None:
                     freq = Utils.FREQUENCY_MAP[freq]
                 else:
                     freq = '1Y'
 
+                # Parse data one variable at time
                 df = pd.DataFrame()
                 for row in range(len(data)):
                     series = data.iloc[row, :]
+                    # Find series name, values and starting timestamp
                     series_name = series.loc['series_name']
-                    start_timestamp = series.loc['start_timestamp']
                     values = series.loc['value']
+                    if 'start_timestamp' in data.columns:
+                        start_timestamp = series.loc['start_timestamp']
+                    else:
+                        start_timestamp = Utils.default_start_timestamp
 
+
+                    # Format and apply date range index
                     column = pd.DataFrame({series_name: values})
                     timestamps = pd.date_range(start=start_timestamp, periods=len(column), freq=freq)
                     column = column.set_index(timestamps)
 
+                    # Join to main dataframe
                     df = pd.concat([df, column], axis=1)
                 df.to_csv(csv_path)
             else:

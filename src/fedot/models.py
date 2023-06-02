848397890c55d803d6dcc298655da28a4b175502
Christian O'Leary <christian.oleary@mtu.ie>
2023-06-02 12:06:14 +0100

    correct limit for FEDOT

--- src/fedot/models.py
+++ src/fedot/models.py
@@ -40,8 +40,7 @@ class FEDOTForecaster(Forecaster):
         model = Fedot(problem='ts_forecasting',
                     task_params=task.task_params,
                     # use_input_preprocessing=True, # fedot>=0.7.0
-                    # timeout=limit, # minutes
-                    timeout=1, # minutes
+                    timeout=limit, # minutes
                     preset=preset,
                     seed=limit,
                     )
@@ -59,16 +58,7 @@ class FEDOTForecaster(Forecaster):
         # else:
         #     model.import_as_project(project_path=os.path.join(tmp_dir, 'project.zip'))
 
-        print('y_train', y_train.shape)
-        print('X_train', X_train.shape)
-        print('X_test', X_test.shape)
-        # predictions = model.forecast(X_test)
-        # predictions = model.predict(features=X_test)
         predictions = self.rolling_origin_forecast(model, X_train, X_test, horizon)
-        # ValueError: all the input arrays must have same number of dimensions, but the array
-        # at index 0 has 2 dimension(s) and the array at index 1 has 1 dimension(s)
-        print('predictions', predictions, type(predictions))
-
         return predictions
 
 

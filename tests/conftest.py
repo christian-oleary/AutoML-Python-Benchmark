7e0826caf20c684b50a1af200db83980d5379192
Christian O'Leary <christian.oleary@mtu.ie>
2025-01-24 12:19:25 +0000

    Avoid caching if checksum error

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,8 @@
 """Functions for setting up and tearing down tests."""
 
 from __future__ import annotations
+
+import os
 from pathlib import Path
 
 import pandas as pd
@@ -17,27 +19,8 @@ def download_bike_sharing(
     :param str | Path | None csv_path: Path to save the dataset as a CSV file, or None to not save.
     :return pd.DataFrame df: Bike sharing dataset.
     """
-
-    def download(**kwargs):
-        return fetch_openml('Bike_Sharing_Demand', version=2, as_frame=True, **kwargs)
-
     # Try to download the dataset
-    try:
-        bike_sharing = bike_sharing = download(parser='auto')
-    # Checksum error
-    except ValueError:
-        # text = error.args[0]
-        # if 'checksum' in text:
-        #     logger.warning(text)
-        bike_sharing = download(cache=False)
-        # else:
-        #     raise error
-    # Newer versions of scikit-learn
-    except TypeError:
-        bike_sharing = download()
-
-    # Get the dataset as a pandas DataFrame
-    df = bike_sharing.frame
+    df = attempt_download('Bike_Sharing_Demand', 2)
 
     # Limit the number of rows
     if row_limit is not None:
@@ -54,4 +37,35 @@ def download_bike_sharing(
     # Save the dataset
     if csv_path is not None:
         df.to_csv(csv_path, index=False)
+    return df
+
+
+def attempt_download(dataset_name: str, version: int) -> pd.DataFrame:
+    """Attempt to download a dataset from OpenML.
+
+    :param str dataset_name: Name of the dataset.
+    :param int version: Version of the dataset.
+    :return pd.DataFrame df: Dataset.
+    """
+    kwargs = {'name': dataset_name, 'version': version, 'as_frame': True}
+    # Try to download the dataset
+    try:
+        data = fetch_openml(**kwargs, parser='auto')
+    except ValueError as e:
+        # Checksum error
+        if 'md5 checksum' in str(e):
+            # Clear the cache and retry download
+            cache_path = os.path.expanduser('~/scikit_learn_data')
+            if os.path.exists(cache_path):
+                for root, _, files in os.walk(cache_path):
+                    for file in files:
+                        os.remove(os.path.join(root, file))
+            data = fetch_openml(**kwargs, cache=True)
+        else:
+            raise e
+    except TypeError:
+        # Newer versions of scikit-learn
+        data = fetch_openml(**kwargs)
+    # Get the dataset as a pandas DataFrame
+    df = data.frame
     return df
